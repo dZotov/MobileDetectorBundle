0d91840ec382ad6856e60ece330647dc3f2f9ef4
dZotov <for.zotov@gmail.com>
2012-03-09 23:59:15 +0400

    updates

--- EventListener.php
+++ EventListener.php
@@ -10,13 +10,13 @@ class EventListener {
 
     protected $container;
 
-    public function __construct(ContainerInterface $container){
+    public function __construct(ContainerInterface $container) {
         $this->container = $container;
     }
 
-    public function onKernelRequest(GetResponseEvent $event){
+    public function onKernelRequest(GetResponseEvent $event) {
         $detector = $this->container->get('app.mobiledetectorbundle.mobile_detector');
-        if ($result = $detector->detect()){
+        if ($result = $detector->detect()) {
             if (!is_array($result['redirect_params']) || empty($result['redirect_params']['redirect_url'])) return;
             $event->setResponse(new RedirectResponse($result['redirect_params']['redirect_url'], $result['redirect_params']['status_code'] ? : 302));
         }
